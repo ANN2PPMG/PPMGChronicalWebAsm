f90990ce8f91b8c641494ed0b80bda40153c18fa
atanas.nikolov74@gmail.com <nzswyHBq93PAMv>
2025-05-06 14:49:55 +0300

    Updated base address, added PMGChronical.db to wwwroot, and fixed deployment issues

--- Program.cs
+++ Program.cs
@@ -15,13 +15,22 @@ namespace PPMGChronicalWebAsm
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            var baseAddress = builder.HostEnvironment.BaseAddress;
-            if (baseAddress.EndsWith("/"))
-            {
-                baseAddress = baseAddress.TrimEnd('/') + "/PPMGChronicalWebAsm/"; // Replace with your repository name
-            }
+            //var baseAddress = builder.HostEnvironment.BaseAddress;
+            //if (baseAddress.EndsWith("/"))
+            //{
+            //    baseAddress = baseAddress.TrimEnd('/') + "/PPMGChronicalWebAsm/"; // Replace with your repository name
+            //}
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // Dynamically set the BaseAddress
+            var isRunningLocally = builder.HostEnvironment.BaseAddress.Contains("localhost");
+            var baseAddress = isRunningLocally
+                ? builder.HostEnvironment.BaseAddress // Use localhost for local development
+                : "https://ann2ppmg.github.io/PPMGChronicalWebAsm/"; // Use GitHub Pages base address
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
             builder.Services.AddScoped<SqliteService>();
             builder.Services.AddSingleton<ErrorService>();
 
