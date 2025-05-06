7617fa273f084c034fe5483bf10c302a3541ca05
ANN2PPMG <ANN2PPMG@users.noreply.github.com>
2025-05-06 06:00:37 +0000

    deploy: bd6a0a2fc70c75d5c87fc9f314604972b64a63f5

---
+++ js/sqliteinterop.js
@@ -0,0 +1,176 @@
+﻿var db;
+
+// Initialize the database
+window.initSqlDb = async function (dbPath) {
+    const SQL = await initSqlJs({ locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/${file}` });
+
+    // Fetch the pre-populated database file
+    const response = await fetch(dbPath);
+    const arrayBuffer = await response.arrayBuffer();
+    const uInt8Array = new Uint8Array(arrayBuffer);
+
+    // Open the database
+    db = new SQL.Database(uInt8Array);
+    console.log("SQLite database initialized");
+};
+
+// Execute queries
+window.executeSqlQuery = function (query) {
+    if (!db) {
+        throw "Database not initialized";
+    }
+
+    const results = db.exec(query);
+    console.log("Query executed:", query);
+    console.log("Results:", results);
+    if (results.length === 0) {
+        return [];
+    }
+
+    // Convert results to array format
+    let values = results[0].values.map(row => row.map(value => value !== null ? value.toString() : ""));
+    console.log(values);
+    return values;
+};
+
+window.getColumnNames = function (tableName) {
+    if (!db) {
+        throw "Database not initialized";
+    }
+    const query = "SELECT name FROM pragma_table_info('" + tableName + "');";
+    const results = db.exec(query);
+    console.log("Query executed:", query);
+    console.log("Results:", results);
+
+    if (results.length === 0) {
+        return [];
+    }
+
+    // Extract just the column names as a simple string array
+    const columnNames = results[0].values;
+    console.log("Column names:", columnNames);
+    return columnNames;
+};
+
+// Add this to allow saving changes to the database
+window.exportDatabaseToFile = function () {
+    if (!db) {
+        throw "Database not initialized";
+    }
+
+    // Export the database to a Uint8Array
+    const data = db.export();
+
+    // Create a blob and download it
+    const blob = new Blob([data], { type: "application/octet-stream" });
+    const url = URL.createObjectURL(blob);
+
+    // Create a link and trigger the download
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "PMGChronical.db";
+    a.click();
+
+    // Clean up
+    URL.revokeObjectURL(url);
+
+    return true;
+};
+
+// Create a new competition
+window.createCompetition = function (competition) {
+    if (!db) {
+        throw "Database not initialized";
+    }
+
+    const query = `
+        INSERT INTO Competitions (id, name, description, subject, period_from, period_to, tag, school_year)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+    `;
+    const stmt = db.prepare(query);
+    stmt.run([
+        competition.id,
+        competition.name,
+        competition.description,
+        competition.subject,
+        competition.period_from,
+        competition.period_to,
+        competition.tag,
+        competition.school_year
+    ]);
+    stmt.free();
+    console.log("Competition created:", competition);
+};
+
+// Read all competitions
+window.readCompetitions = function () {
+    if (!db) {
+        throw "Database not initialized";
+    }
+
+    const query = "SELECT * FROM Competitions;";
+    const results = db.exec(query);
+    console.log("Competitions read:", results);
+
+    if (results.length === 0) {
+        return [];
+    }
+
+    return results[0].values.map(row => row.map(value => value !== null ? value.toString() : ""));
+};
+
+// Update a competition
+window.updateCompetition = function (competition) {
+    if (!db) {
+        throw "Database not initialized";
+    }
+
+    const query = `
+        UPDATE Competitions
+        SET name = ?, description = ?, subject = ?, period_from = ?, period_to = ?, tag = ?, school_year = ?
+        WHERE id = ?;
+    `;
+    const stmt = db.prepare(query);
+    stmt.run([
+        competition.name,
+        competition.description,
+        competition.subject,
+        competition.period_from,
+        competition.period_to,
+        competition.tag,
+        competition.school_year,
+        competition.id
+    ]);
+    stmt.free();
+    console.log("Competition updated:", competition);
+};
+
+// Delete a competition
+window.deleteCompetition = function (id) {
+    if (!db) {
+        throw "Database not initialized";
+    }
+
+    const query = "DELETE FROM Competitions WHERE id = ?;";
+    const stmt = db.prepare(query);
+    stmt.run([id]);
+    stmt.free();
+    console.log("Competition deleted with id:", id);
+};
+
+window.showModal = (modalId) => {
+    const modalElement = document.getElementById(modalId);
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+    return true;
+};
+
+window.hideModal = (modalId) => {
+    const modalElement = document.getElementById(modalId);
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    if (modal) {
+        modal.hide();
+        return true;
+    }
+    return false;
+};
---
+++ service-worker-assets.js
@@ -0,0 +1,249 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-yis2dNdJ6e9R472mcuwUOsxBPCQX3snSMmHAL3zu2D8=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-z8OR40MowJ8GgK6P89Y+hiJK5+cclzFHzLhFQLL92bg=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-gBwg2tmA0Ci2u54gMF1jNCVku6vznarkLS6D76htNNQ=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-4mWsDy3aHl36ZbGt8zByK7Pvd4kRUoNgTYzRnwmPHwg=",
+      "url": "favicon.png"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk\/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-yPWewRBtq2bcoJAxUlP\/ebRSLgAj3dUe8vMbVYe\/kdg=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-4HBFwIwzQeDIixU5UkKN0lpSgq8dw+iK4RDfy2UHAB0=",
+      "url": "js\/sqliteinterop.js"
+    },
+    {
+      "hash": "sha256-ZwFzGeUW0IDj7o4a2WeVA\/NICpNQWPgH+kb5OKJtvBE=",
+      "url": "manifest.webmanifest"
+    },
+    {
+      "hash": "sha256-paIeuWnSzjeLGITWkk4p3rcejUsZWCJGPeOK9UhWXtg=",
+      "url": "sample-data\/weather.json"
+    },
+    {
+      "hash": "sha256-gvHfnndfEu1tRf0rFb5988rWq7ITIotOaE8+AMbKYbc=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-Ld2mMcflCWWH+RENVpZ2f+7zV+JJowPzvuzSrrHgLhI=",
+      "url": "_framework\/dotnet.js"
+    },
+    {
+      "hash": "sha256-ftpNqtARtVNE8ku5eB\/jXoklb41UQ+EutUHfwmbNTBY=",
+      "url": "_framework\/dotnet.native.8.0.15.l5bze06whq.js"
+    },
+    {
+      "hash": "sha256-WlcCPeEKcjzreT7MGsFKQGVq8TKP2f2vhVBU9lrBv1s=",
+      "url": "_framework\/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-FDzrAx2UjG21VHaNZAes6hQGG2S\/oq5S2VaFjEBfPgM=",
+      "url": "_framework\/dotnet.runtime.8.0.15.c4prjo5hgp.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework\/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework\/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37\/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework\/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-stjezfvp7BSo34QmMHSur\/DcZFgRlrlqS8Ov2BzdPf4=",
+      "url": "_framework\/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-eFBnBx0SZSsywfiHkr\/ICGB4vhPgDjDkGX+aGXw6ZsY=",
+      "url": "_framework\/Markdig.wasm"
+    },
+    {
+      "hash": "sha256-Yyn742T5ixKT8wJBGzU19xs1sadMwWLOOc7tJatmrJk=",
+      "url": "_framework\/Microsoft.AspNetCore.Authorization.wasm"
+    },
+    {
+      "hash": "sha256-0npONCRnS\/9v65EOrnB5d+0sTnTgeQo6uMrtscRATuo=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Authorization.wasm"
+    },
+    {
+      "hash": "sha256-SEjd4R9MmQjQgaeDLYyBGp98yyq9v+ivFlE8elQy3oM=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-6H6sw6DPC\/l2ZCZQ6FDUbgopODrSc3TILZkNKjEFApw=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-fBqw1fwDIGHXEilpEF3XAl1GEIGrg\/Mee9XcqCb473o=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-4ZNcT2nLRt93K01msmzpknW\/bi0FG0Yoij4IXa84tC0=",
+      "url": "_framework\/Microsoft.AspNetCore.Metadata.wasm"
+    },
+    {
+      "hash": "sha256-87sn2TYqgdZ95sXmavjKEzoEfMgHnYQ9LOvnMX+aZcI=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-\/VUEfhgVp9FCU1rXnSNAI1mtW6EO5DpkV\/5UNWDR\/es=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-jYqHUZ07UYWc8POk7xhas6xQYH7t1qoTcylqoDqncJk=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-1sB9RS3Mloi+A0EwqAAdJWdh91DHciC5BfhShGArmUM=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-T\/+qPF0RWC8dfbLNFgoTfla+JM2H3eY5L17aqODwzFc=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-IdwTf3t0aa2CzeTFazZZoL23SalxMkjF5JAhHeUfOgY=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-gRNqDIITN9\/LJz459bgijHJ1O+l5fzsybyrFhw0Iqa4=",
+      "url": "_framework\/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-ezKJDNjeghS\/qMdQmUpBPdH7bCDLrqkNqS6RsLxlpOY=",
+      "url": "_framework\/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-6187ynEahDlSLMBvD4vAmiLpZ3clRb5xu6rM7O8AxNo=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-zFg0Y1pJ9elYqhBThL6jLvY00t6ALHU6rfiyc4buWOQ=",
+      "url": "_framework\/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-g67T8DzNXo4jgqaEAc5IV2geLKB19sYEWN3rozAJzm0=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-Xhr+BwtakLrRUMklPBXkiWY17IY5oAzWRBKnTLTCEPA=",
+      "url": "_framework\/PPMGChronicalWebAsm.wasm"
+    },
+    {
+      "hash": "sha256-NvtX1U3u9caaD41QENP9zGZHkgITcEj8+1O0Foq4dkk=",
+      "url": "_framework\/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-fmJVPK1JfSI4Pql9Nb3FDhQZV5rilJTJnxpRBEOf38A=",
+      "url": "_framework\/System.Collections.Immutable.wasm"
+    },
+    {
+      "hash": "sha256-UNqsVAJN\/6KiGZH69U04fJzkJK3F1K7tM8sFOXrFS7g=",
+      "url": "_framework\/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-uioW3YAgF0V59CXuySz24fyzL\/jhbqrRLWYiDaZ1BZ4=",
+      "url": "_framework\/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-LX7x9EIjpwqHyDyvhPNoMp1+NDFUbKX4YYE0H5RjN4U=",
+      "url": "_framework\/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-ckbl0IuDRu\/PDB\/OkTCIwMMt\/+CLZiPMcZrDvJrhnVY=",
+      "url": "_framework\/System.Diagnostics.DiagnosticSource.wasm"
+    },
+    {
+      "hash": "sha256-j+xugwrg+4uy29siEp4hAVpjlOq9HBIipH0i+pBpdIw=",
+      "url": "_framework\/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-5RV5Mqv77mhyC79HYVoUpxgejRL2HuuKDZ1sUhL3eZo=",
+      "url": "_framework\/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-5NL+iGQojq2kMm5Zkyht1RekJj5lYv5mJ2JXVk0rSAU=",
+      "url": "_framework\/System.Net.Http.Json.wasm"
+    },
+    {
+      "hash": "sha256-Ug9XH2QDPI1jq9auVsIX2VMN54om6ezm0rh6Qhujb94=",
+      "url": "_framework\/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-8ZWYuLg0pmFOHG\/yivISF7mfMgRN93tuk2\/T8IkDkEw=",
+      "url": "_framework\/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-EDVVe3hyTmEgtPM9UbP2D1G9DEv7O1BmwSeMXn2kjC0=",
+      "url": "_framework\/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-wHw9QhgSVTim9g7A9v0lb2UZmnfIzma81hjiW6ObYFA=",
+      "url": "_framework\/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-ztXzc7ufqw28jhHoyLr1SqRrRpZ+dYqlPxeiahcsD8g=",
+      "url": "_framework\/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-rvjT\/tYBQniM8Risr97OtznXM\/ggOblQXfWXYvX2KRM=",
+      "url": "_framework\/System.Runtime.InteropServices.wasm"
+    },
+    {
+      "hash": "sha256-tb5zFVJrsSt4a31Ie5aK1la4IReiKYevIJbif\/qBV0E=",
+      "url": "_framework\/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-JQAhREkv1RKmEp2BDQbyksi8VQ6Rzq2wvld9dCHe\/Uw=",
+      "url": "_framework\/System.Security.Claims.wasm"
+    },
+    {
+      "hash": "sha256-MsEppwsm23ec2KJNhzQXknAXF0MwxFv2uYHzKddAHNM=",
+      "url": "_framework\/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-nSzZa77obeu6c001us8eLu+\/3pO3DSK+oCO7aqTI2lk=",
+      "url": "_framework\/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-HcZuePR4sy2OeN6Nt9pFlweGqXET8\/4km86NaeWHBCs=",
+      "url": "_framework\/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-IYYG5507XelY2I1AXLxbil\/+arj6Df4s2dZHPsbB0K4=",
+      "url": "_framework\/System.Threading.wasm"
+    },
+    {
+      "hash": "sha256-tBEwPFfSFblA0VSTsvjz71cekDi1DXl++dt+kzfLMJ8=",
+      "url": "PPMGChronicalWebAsm.styles.css"
+    },
+    {
+      "hash": "sha256-Qlj6NGS978hjVyrYLtW5G6trKGerdW4Fwgvr2eUo\/lU=",
+      "url": "_content\/Microsoft.AspNetCore.Components.WebAssembly.Authentication\/AuthenticationService.js"
+    }
+  ],
+  "version": "+a69NyGn"
+};
